bb212e2bb351faf289f2d129c2e5537eed4e38dc
BaptisteMchlt <baptistemchlt@gmail.com>
2023-03-09 17:08:07 +0100

    utilisateur+role

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -5,16 +5,17 @@ import { FormsModule } from '@angular/forms';
 
 import { AdminLayoutRoutes } from './admin-layout.routing';
 
-import { DashboardComponent }       from '../../pages/dashboard/dashboard.component';
-import { UserComponent }            from '../../pages/user/user.component';
-import { TableComponent }           from '../../pages/table/table.component';
-import { TypographyComponent }      from '../../pages/typography/typography.component';
-import { IconsComponent }           from '../../pages/icons/icons.component';
-import { MapsComponent }            from '../../pages/maps/maps.component';
-import { NotificationsComponent }   from '../../pages/notifications/notifications.component';
-import { UpgradeComponent }         from '../../pages/upgrade/upgrade.component';
+import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
+import { UserComponent } from '../../pages/user/user.component';
+import { TableComponent } from '../../pages/table/table.component';
+import { TypographyComponent } from '../../pages/typography/typography.component';
+import { IconsComponent } from '../../pages/icons/icons.component';
+import { MapsComponent } from '../../pages/maps/maps.component';
+import { NotificationsComponent } from '../../pages/notifications/notifications.component';
+import { UpgradeComponent } from '../../pages/upgrade/upgrade.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UtilisateurComponent } from 'app/pages/utilisateur/utilisateur.component';
 
 @NgModule({
   imports: [
@@ -32,7 +33,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     IconsComponent,
     MapsComponent,
     NotificationsComponent,
+    UtilisateurComponent
   ]
 })
 
-export class AdminLayoutModule {}
+export class AdminLayoutModule { }
---
+++ src/app/models/utilisateur.ts
@@ -0,0 +1,13 @@
+import { Role } from "./role";
+//import { Tache } from "./tache";
+
+export class Utilisateur {
+    idUtilisateur!: number;
+    nomUtilisateur!: string;
+    prenomUtilisateur!: string;
+    emailUtilisateur!: string;
+    roles!: Role[];
+    //taches!: Tache[];
+    username!: string;
+    password!: string;
+}
