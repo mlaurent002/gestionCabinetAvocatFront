1931ecaf84488a8bf082f96c94d08211851c8b1e
BaptisteMchlt <baptistemchlt@gmail.com>
2023-03-10 09:43:58 +0100

    ajout formulaireUtilisateur

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -10,7 +10,7 @@ export interface RouteInfo {
 
 export const ROUTES: RouteInfo[] = [
 
-    //{ path: '/affaire', title: 'Affaires', icon: 'nc-box', class: '' },
+    { path: '/affaire', title: 'Affaires', icon: 'nc-box', class: '' },
     { path: '/dashboard', title: 'Dashboard', icon: 'nc-bank', class: '' },
     { path: '/icons', title: 'Icons', icon: 'nc-diamond', class: '' },
     { path: '/maps', title: 'Maps', icon: 'nc-pin-3', class: '' },
@@ -19,7 +19,7 @@ export const ROUTES: RouteInfo[] = [
     { path: '/table', title: 'Table List', icon: 'nc-tile-56', class: '' },
     { path: '/typography', title: 'Typography', icon: 'nc-caps-small', class: '' },
     { path: '/upgrade', title: 'Upgrade to PRO', icon: 'nc-spaceship', class: 'active-pro' },
-    //{ path: '/utilisateur', title: 'Gestion des utilisateurs', icon: 'nc-diamond', class: '' },
+    { path: '/utilisateur', title: 'Gestion des utilisateurs', icon: 'nc-diamond', class: '' },
 
 ];
 
